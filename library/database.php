d3ff0231c7c8071e686539bbad9eaf4c3746724c
fenicmmoi <phatthalung.it@gmail.com>
2020-10-20 16:20:45 +0700

    hide password

--- library/database.php
+++ library/database.php
@@ -12,22 +12,14 @@ $dbConn->select_db($dbName);
 */
 
 $db_config=array(
-        "host"=>"localhost",  // กำหนด host
-        "user"=>"root", // กำหนดชื่อ user
-        "pass"=>"hellojava",   // กำหนดรหัสผ่าน
-        "dbname"=>"stock",  // กำหนดชื่อฐานข้อมูล
+        "host"=>"",  // กำหนด host
+        "user"=>"", // กำหนดชื่อ user
+        "pass"=>"",   // กำหนดรหัสผ่าน
+        "dbname"=>"",  // กำหนดชื่อฐานข้อมูล
         "charset"=>"utf8"  // กำหนด charset
     );
 
-/*
-    $db_config=array(
-        "host"=>"localhost",  // กำหนด host
-        "user"=>"phatthalun_dol", // กำหนดชื่อ user
-        "pass"=>"nSSYV5cJ",   // กำหนดรหัสผ่าน
-        "dbname"=>"phatthalun_stock",  // กำหนดชื่อฐานข้อมูล
-        "charset"=>"utf8"  // กำหนด charset
-    );
-*/
+
 
 $dbConn = @new mysqli($db_config["host"], $db_config["user"], $db_config["pass"], $db_config["dbname"]);
 if(mysqli_connect_error()) {
