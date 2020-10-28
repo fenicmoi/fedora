cd1ed71998dd1f125e19121c96b71401c0de09cb
fenicmmoi <phatthalung.it@gmail.com>
2020-10-28 14:47:58 +0700

    number format

--- list.php
+++ list.php
@@ -55,7 +55,7 @@ include("navbar.php");
                                                 <td>".$row['listname']."</td>
                                                 <td>".$row['moneyID']."</td>
                                                 <td>".$row['descript']."</td>
-                                                <td>".format_number($row['amount'])."</td>
+                                                <td>".number_format($row['amount'])."</td>
                                                 <td>".$row['price']."</td>
                                                 <td>".$row['howto']."</td>
                                                 <td>".$row['reciveDate']."</td>
