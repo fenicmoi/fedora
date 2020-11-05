99fb101325f1ac105b15f02ec223dc6f45fbbae4
fenicmmoi <phatthalung.it@gmail.com>
2020-11-05 17:20:08 +0700

    เพิ่มการลยและ sweetalert

--- sub_project.php
+++ sub_project.php
@@ -472,36 +472,49 @@ $(document).ready(function(){
         // Delete id
         var id = $(this).data('id');
         
-        var confirmalert = confirm("Are you sure?");
+        Swal.fire({
+            title: 'กำลังจะลบข้อมูล?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'ใช่, ลบมันเลย!',
+            cancelButtonText: 'ไม่ลบ'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: 'remove-subproject.php',
+                    type: 'POST',
+                    data: { id:id },
+                    success: function(response){
+
+
+                        if(response == 1){
+                            // Remove row from HTML Table
+                            $(el).closest('tr').css('background','tomato');
+                            $(el).closest('tr').fadeOut(800,function(){
+                                $(this).remove();
+                            });
+                            
+                        }else{
+                        // alert('Invalid ID.'+id);
+                        Swal.fire({
+                                icon: 'error',
+                                title: 'อุ๊บบ...',
+                                text: 'มีบางอย่างผิดพลาด!',
+                                footer: '<a href>ติดต่อ admin</a>'
+                            })
+                        }
+
+
+                    }
+                });
+            }
+        })
+        
         if (confirmalert == true) {
             // AJAX Request
-            $.ajax({
-                url: 'remove-subproject.php',
-                type: 'POST',
-                data: { id:id },
-                success: function(response){
-
-
-                    if(response == 1){
-                        // Remove row from HTML Table
-                        $(el).closest('tr').css('background','tomato');
-                        $(el).closest('tr').fadeOut(800,function(){
-                            $(this).remove();
-                        });
-                        
-                    }else{
-                       // alert('Invalid ID.'+id);
-                       Swal.fire({
-                            icon: 'error',
-                            title: 'อุ๊บบ...',
-                            text: 'มีบางอย่างผิดพลาด!',
-                            footer: '<a href>ติดต่อ admin</a>'
-                        })
-                    }
-
-
-                }
-            });
+            cosole.log('hello')
         }
     });
 });
